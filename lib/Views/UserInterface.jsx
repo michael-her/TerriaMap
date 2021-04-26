1e6d4edc3fe50fb2ebd1c47e1bcf2f0fd777fa36
Sehui Min <dr.michael.her@gmail.com>
2021-04-26 10:15:36 +0900

    JSX error fix

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -40,23 +40,23 @@ function isBrowserSupportedAV() {
 export default function UserInterface(props) {
   return (
     <StandardUserInterface {...props} version={version}>
-      <Menu>
-        {/* <RelatedMaps viewState={props.viewState} /> */}
-        {/* <MenuItem caption="정보" href="about.html" key="about-link" /> */}
-      </Menu>
-      <Nav>
-        {/* <MeasureTool terria={props.viewState.terria} key="measure-tool" /> */}
-      </Nav>
-      <ExperimentalMenu>
-        {/* <If condition={isBrowserSupportedAV()}>
+      {/* <Menu>
+        <RelatedMaps viewState={props.viewState} />
+        <MenuItem caption="정보" href="about.html" key="about-link" />
+      </Menu> */}
+      {/* <Nav>
+        <MeasureTool terria={props.viewState.terria} key="measure-tool" />
+      </Nav> */}
+      {/* <ExperimentalMenu>
+        <If condition={isBrowserSupportedAV()}>
           <SplitPoint
             loadComponent={loadAugmentedVirtuality}
             viewState={props.viewState}
             terria={props.viewState.terria}
             experimentalWarning={true}
           />
-        </If> */}
-      </ExperimentalMenu>
+        </If>
+      </ExperimentalMenu> */}
     </StandardUserInterface>
   );
 }
